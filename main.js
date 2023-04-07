121b5d6d4499c5323ee920eef2d99f7185513693
Bojan <bojanf.1996@gmail.com>
2023-04-07 20:47:32 +0200

    Devel (#3)
    * template page
    
    * some basic chat
    
    * input message trimming
    
    * better input box

---
+++ main.js
@@ -0,0 +1,100 @@
+
+if (false) {
+    var is_key_pressed = true;
+
+    // wait for document to load
+    document.addEventListener('DOMContentLoaded', function () {
+        // get app id from html
+        var app = document.getElementById('app');
+        if (is_key_pressed) {
+            app.innerHTML = 'Hello World';
+        }
+        else {
+            app.innerHTML = 'Goodbye World';
+        }
+
+        /*
+        add_chat_message("hello world");
+        add_user_message("no hello world");
+    
+        add_chat_message("hello world 2");
+        //add_user_message("no hello world 4");
+        */
+    });
+}
+
+/* on page load */
+document.addEventListener('DOMContentLoaded', function () {
+    /* add event listener to message when enter is pressed */
+    document.getElementById('message').addEventListener('keyup', function (e) {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            on_send_button();
+        }
+    });
+});
+
+function add_chat_message(text) {
+    var chat = document.getElementById('chat_history');
+
+    // make box 1 and append to chat
+    var msg_user = document.createElement('div');
+    msg_user.className = 'msg_bot';
+    var msg_icon = document.createElement('img');
+    msg_icon.src = 'images/gpt4.png';
+    msg_icon.alt = 'gpt';
+    msg_icon.className = 'msg_icon';
+    msg_user.appendChild(msg_icon);
+
+    // add paragraph
+    var msg_text = document.createElement('p');
+    msg_text.className = 'msg_text';
+    msg_text.innerHTML = text;
+    msg_user.appendChild(msg_text);
+
+    msg_user.appendChild(msg_text);
+    chat.appendChild(msg_user);
+}
+
+function add_user_message(text) {
+    var chat = document.getElementById('chat_history');
+
+    // make box 1 and append to chat
+    var msg_user = document.createElement('div');
+    msg_user.className = 'msg_user';
+    var msg_icon = document.createElement('img');
+    msg_icon.src = 'images/user.png';
+    msg_icon.alt = 'gpt';
+    msg_icon.className = 'msg_icon';
+    msg_user.appendChild(msg_icon);
+    
+    // add paragraph
+    var msg_text = document.createElement('p');
+    msg_text.className = 'msg_text';
+    msg_text.innerHTML = text;
+    msg_user.appendChild(msg_text);
+
+    chat.appendChild(msg_user);
+}
+
+var tmp = 3;
+
+function on_send_button() {
+    // get from text from message
+    var msg = document.getElementById('message');
+    msg.value = msg.value.trim();
+
+    /* check if message is empty */
+    if (msg.value == "") {
+        return;
+    }
+
+    console.log(msg.value);
+
+    // add user message to chat
+    add_user_message(msg.value);
+    msg.value = "";
+
+    // add bot message to chat
+    add_chat_message("hello world " + tmp);
+    tmp += 1;
+}
